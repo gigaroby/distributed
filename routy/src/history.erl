4b95818d79e5740061662d0b278ade54487dfebc
Roberto Bampi <bampi.roberto@gmail.com>
2016-09-21 16:42:30 +0200

    makefile

--- routy/src/history.erl
+++ routy/src/history.erl
@@ -1,11 +1,11 @@
 -module(history).
 -export([new/1, update/3]).
 
-new(Name) -> dict:set(Name, -1, dict:new()).
+new(Name) -> dict:store(Name, -1, dict:new()).
 
 update(Node, N, History) ->
 	case dict:find(Node, History) of
 		{ok, Age} when Age >= N -> old; 
-		_ -> {new, dict:set(Node, N, History)}
+		_ -> {new, dict:store(Node, N, History)}
 	end.
 
